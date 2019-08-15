cc90a1f2afc04b546c2971fc70aae3293743d836
millefalcon <hanish0019@gmail.com>
2019-08-15 16:11:30 +0530

    Added pysh.bin to packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pysh',
       version='0.1.0',
-      packages=['pysh'],
+      packages=['pysh', 'pysh.bin'],
       entry_points={
           'console_scripts': [
               'pysh = pysh.__main__:main'
